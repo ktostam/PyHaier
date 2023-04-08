4c38f111bcc6ff81a15f602da8e879ef199ef910
Jacek Brzozowski <jacek.brzozowski@adm.uw.edu.pl>
2023-04-08 11:48:29 +0200

    update

--- PyHaier/__init__.py
+++ PyHaier/__init__.py
@@ -1,6 +1,7 @@
 from PyHaier.GetCHTemp import GetCHTemp
 from PyHaier.SetCHTemp import SetCHTemp
 from PyHaier.GetMode import GetMode
+from PyHaier.SetMode import SetMode
 from PyHaier.GetDHWTemp import GetDHWTemp
 from PyHaier.SetDHWTemp import SetDHWTemp
 from PyHaier.GetState import GetState
