7a2f74f291f320902c01ba2e8c84c8ea855673c7
Jacek Brzozowski <jacek.brzozowski@adm.uw.edu.pl>
2023-04-08 11:39:35 +0200

    update

---
+++ PyHaier/SetDHWTemp.py
@@ -0,0 +1,11 @@
+def SetDHWTemp(current,new):
+
+    newtemp = []
+    newtemp.append(int(hex(current[0])[4:6], 16))
+    newtemp.append(current[1])
+    newtemp.append(int(hex(current[2])[2:6], 16))
+    newtemp.append(int(hex(current[3])[4:6], 16))
+    newtemp.append(512 + int(hex(current[4])[4:6], 16))
+    newtemp.append(int(str(hex(int(new * 2)) + hex(current[5])[4:6]), 16))
+
+    return newtemp
