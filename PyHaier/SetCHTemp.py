5a751af620485cc148d83c9046db5a1d8483182a
Jacek Brzozowski <jacek.brzozowski@adm.uw.edu.pl>
2023-04-08 11:34:59 +0200

    update

--- PyHaier/SetCHTemp.py
+++ PyHaier/SetCHTemp.py
@@ -1,1 +1,12 @@
 
+def SetCHTemp(current,new):
+
+    newtemp = []
+    newtemp.append(1024 + int(hex(current[0])[4:6], 16))
+    newtemp.append(int(str(hex(int(new * 2)) + hex(current[1])[4:6]), 16))
+    newtemp.append(int(hex(current[2])[2:6], 16))
+    newtemp.append(int(hex(current[3])[4:6], 16))
+    newtemp.append(int(hex(current[4])[4:6], 16))
+    newtemp.append(current[5])
+
+    return newtemp
--- PyHaier/__init__.py
+++ PyHaier/__init__.py
@@ -1,4 +1,6 @@
 from PyHaier.GetCHTemp import GetCHTemp
+from PyHaier.SetCHTemp import SetCHTemp
 from PyHaier.GetMode import GetMode
 from PyHaier.GetDHWTemp import GetDHWTemp
 from PyHaier.GetState import GetState
+from PyHaier.SetState import SetState
