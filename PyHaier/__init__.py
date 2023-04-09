d26e9093980bd1cd031b02091f835472ffad9c45
Jacek Brzozowski <jacek.brzozowski@adm.uw.edu.pl>
2023-04-09 08:56:03 +0200

    Add function to display current termperature in tank

--- PyHaier/__init__.py
+++ PyHaier/__init__.py
@@ -3,6 +3,7 @@ from PyHaier.SetCHTemp import SetCHTemp
 from PyHaier.GetMode import GetMode
 from PyHaier.SetMode import SetMode
 from PyHaier.GetDHWTemp import GetDHWTemp
+from PyHaier.GetDHWCurTemp import GetDHWCurTemp
 from PyHaier.SetDHWTemp import SetDHWTemp
 from PyHaier.GetState import GetState
 from PyHaier.SetState import SetState
