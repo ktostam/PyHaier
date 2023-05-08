43221adc06ee912f41a84cf0e88c0a9aad9471ca
Jacek Brzozowski <jacek.brzozowski@adm.uw.edu.pl>
2023-05-08 17:26:08 +0200

    few updates

--- PyHaier/SetCHTemp.py
+++ PyHaier/SetCHTemp.py
@@ -1,12 +1,13 @@
 
 def SetCHTemp(current,new):
-
-    newtemp = []
-    newtemp.append(1024 + int(hex(current[0])[4:6], 16))
-    newtemp.append(int(str(hex(int(new * 2)) + hex(current[1])[4:6]), 16))
-    newtemp.append(int(hex(current[2])[2:6], 16))
-    newtemp.append(int(hex(current[3])[4:6], 16))
-    newtemp.append(int(hex(current[4])[4:6], 16))
-    newtemp.append(current[5])
-
-    return newtemp
+    try:
+        newtemp = []
+        newtemp.append(1024 + int(hex(current[0])[4:6], 16))
+        newtemp.append(int(str(hex(int(int(new) * 2)) + hex(current[1])[4:6]), 16))
+        newtemp.append(int(hex(current[2])[2:6], 16))
+        newtemp.append(int(hex(current[3])[4:6], 16))
+        newtemp.append(int(hex(current[4])[4:6], 16))
+        newtemp.append(current[5])
+        return newtemp
+    except:
+        return "ERROR"
--- PyHaier/SetState.py
+++ PyHaier/SetState.py
@@ -6,42 +6,45 @@ def SetState(current,new):
     :param new:
     :return:
     """
-    newstate=[]
-    if len(str(current[3])) <4 or len(str(current[0])) <4:
-        return "Error"
+    try:
+        newstate=[]
+        if len(str(current[3])) <4 or len(str(current[0])) <4:
+            return "Error"
 
-    if new == "off":
-        if int(hex(current[0])[4:6])%2 == 1:
-            state=int(hex(current[0])[4:6])-1
+        if new == "off":
+            if int(hex(current[0])[4:6])%2 == 1:
+                state=int(hex(current[0])[4:6])-1
+            else:
+                return -1
+        elif new == "on":
+            if int(hex(current[0])[4:6]%2) == 0:
+                state=int(hex(current[0])[4:6])+1
+            else:
+                return -1
+        elif new == "C":
+            state=3
+        elif new == "H":
+            state=5
+        elif new == "T":
+            state=87
+        elif new == "CT":
+            state=83
+        elif new == "HT":
+            state=85
         else:
             return -1
-    elif new == "on":
-        if int(hex(current[0])[4:6]%2) == 0:
-            state=int(hex(current[0])[4:6])+1
+
+        if state%2 == 0:
+            num=256
         else:
-            return -1
-    elif new == "C":
-        state=3
-    elif new == "H":
-        state=5
-    elif new == "T":
-        state=87
-    elif new == "CT":
-        state=83
-    elif new == "HT":
-        state=85
-    else:
-        return -1
+            num=34304
+        newstate.append(num+int(str(int(state)),16))
+        newstate.append(current[1])
+        newstate.append(int(hex(current[2])[2:6], 16))
+        newstate.append(int(hex(current[3])[4:6], 16))
+        newstate.append(int(hex(current[4])[4:6], 16))
+        newstate.append(current[5])
 
-    if state%2 == 0:
-        num=256
-    else:
-        num=34304
-    newstate.append(num+int(str(int(state)),16))
-    newstate.append(current[1])
-    newstate.append(int(hex(current[2])[2:6], 16))
-    newstate.append(int(hex(current[3])[4:6], 16))
-    newstate.append(int(hex(current[4])[4:6], 16))
-    newstate.append(current[5])
-
-    return newstate
+        return newstate
+    except:
+        return "ERROR"
